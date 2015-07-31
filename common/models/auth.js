9b776332ff2d27bac9f3582f59509331a0ace124
Chris Whiten <chris.whiten@gmail.com>
2015-07-31 11:01:56 -0400

    Remove all of the auto-generated endpoints from /auth

--- common/models/auth.js
+++ common/models/auth.js
@@ -7,6 +7,33 @@ var loopback = require('loopback');
 
 module.exports = function (Auth) {
     'use strict';
+
+    Auth.disableRemoteMethod("create", true);
+    Auth.disableRemoteMethod("upsert", true);
+    Auth.disableRemoteMethod("updateAll", true);
+    Auth.disableRemoteMethod("updateAttributes", false);
+
+    Auth.disableRemoteMethod("find", true);
+    Auth.disableRemoteMethod("findById", true);
+    Auth.disableRemoteMethod("findOne", true);
+
+    Auth.disableRemoteMethod("deleteById", true);
+
+    Auth.disableRemoteMethod("confirm", true);
+    Auth.disableRemoteMethod("count", true);
+    Auth.disableRemoteMethod("exists", true);
+    Auth.disableRemoteMethod("resetPassword", true);
+
+    Auth.disableRemoteMethod('__count__accessTokens', false);
+    Auth.disableRemoteMethod('__create__accessTokens', false);
+    Auth.disableRemoteMethod('__delete__accessTokens', false);
+    Auth.disableRemoteMethod('__destroyById__accessTokens', false);
+    Auth.disableRemoteMethod('__findById__accessTokens', false);
+    Auth.disableRemoteMethod('__get__accessTokens', false);
+    Auth.disableRemoteMethod('__updateById__accessTokens', false);
+
+    Auth.disableRemoteMethod('createChangeStream', true);
+
     Auth.validate = function (token, cb) {
         console.log('Validating token: ' + token);
         
