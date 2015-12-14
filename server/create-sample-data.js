663e6c259f356998edacf6a520567795d79542d8
Menan Vadivelpillai <mvadivel@solinkcorp.com>
2015-12-14 17:40:29 -0500

    Taken out POSCamera filters out of the list

--- server/create-sample-data.js
+++ server/create-sample-data.js
@@ -105,7 +105,7 @@ module.exports = function(app, doneCallback) {
           updateUrl: 'http://update.solinkcloud.net',
           checkinInterval: 3600,
           softwareVersionId: results.softwareVersions[0].id,
-          email: 'cwhiten+asdfsasdfasfdsdfff2aasdfsf32asdf13@solinkcorp.com',
+          email: 'cwhiten+asdfsasasdfdfasfdsdfff2aasdfsf32asdf13@solinkcorp.com',
           password: 'test'
         },
         { name: 'Solink APAC',
@@ -115,7 +115,7 @@ module.exports = function(app, doneCallback) {
           updateUrl: 'http://update.solinkcloud.net',
           checkinInterval: 3600,
           softwareVersionId: results.softwareVersions[0].id,
-          email: 'cwhiten+m233asdfasasdfdfasdffsfasdfddfdf3asdf32@solinkcorp.com',
+          email: 'cwhiten+m233asdfasaasdfsdfdfasdffsfasdfddfdf3asdf32@solinkcorp.com',
           password: 'test'
         },
       ], cb);
@@ -132,19 +132,19 @@ module.exports = function(app, doneCallback) {
       app.models.Reseller.create([
         { name: 'Reseller 1',
           cloudId: results.clouds[0].id,
-          email: 'cwhiten+rr2asdf1asasdfdfaaasdf231@solinkcorp.com',
+          email: 'cwhiten+rr2asdf1asasasdfdfdfaaasdf231@solinkcorp.com',
           password: 'test'
         },
         { name: 'Reseller 2',
           cloudId: results.clouds[0].id,
           checkinInterval: 3000,
-          email: 'cwhiten+rr212asdf1asdfaaasdfasdfsdfasf2332@solinkcorp.com',
+          email: 'cwhiten+rr212asdf1asdfaasdfaasdfasdfsdfasf2332@solinkcorp.com',
           password: 'test'
         },
         { name: 'Reseller 3',
           cloudId: results.clouds[1].id,
           checkinInterval: 3000,
-          email: 'cwhiten+rr233asaasasfdfsasasdfasdf2asdf3@solinkcorp.com',
+          email: 'cwhiten+rr233asaasasfdasdffsasasdfasdf2asdf3@solinkcorp.com',
           password: 'test'
         }
       ], cb);
