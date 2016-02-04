e6c83c2fa7b01360bcf5cc3d02a3379d8e2f5ac7
Chris Whiten <chris.whiten@gmail.com>
2016-02-04 12:11:14 -0500

    Swap to d3 + vanilla gmaps

--- client/js/directives/map.js
+++ client/js/directives/map.js
@@ -10,7 +10,6 @@ angular
       link: function (scope, element, attrs) {
         scope.markers = [];
         scope.filteredMarkers = [];
-        scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 4 };
         loadMarkers();
 
         function loadMarkers() {
@@ -19,66 +18,117 @@ angular
               loadMarkers();
             }, 100); // TODO: this is hacky.  how can we get a signal on change?
           } else {
-            scope.devices.forEach(function (device) {
-              if (device.location) {
-                // get device status
-                var lastCheckinTimeInSeconds = new Date(device.lastCheckin).getTime() / 1000;
-                var nowInSeconds = new Date().getTime() / 1000;
-
-                var gracePeriodInSeconds = 60;
-                var hasCheckedInOnTime = (lastCheckinTimeInSeconds + device.checkinInterval + gracePeriodInSeconds) > nowInSeconds;
-
-                var allCamerasOnline = true;
-                if (device.cameras) {
-                  for (var k = 0; k < device.cameras.length; k++) {
-                    var camera = device.cameras[k];
-                    if (camera.status != 'online') {
-                      allCamerasOnline = false;
-                      break;
-                    }
-                  }
-                }
-
-                if (hasCheckedInOnTime) {
-                  if (allCamerasOnline) {
-                    device.status = 'green';
-                  } else {
-                    device.status = 'yellow';
-                  }
-                } else {
-                  device.status = 'red';
-                }
-                var icon = 'assets/images/gmaps_marker_' + device.status + '.png';
-
-                scope.markers.push({
-                  id: device.id,
-                  icon: icon,
-                  latitude: device.location.lat,
-                  longitude: device.location.lng,
-                  showWindow: false,
-                  customerName: device.customerName,
-                  deviceName: device.name,
-                  deviceId: device.id,
-                  options: {
-                    labelAnchor: "22 0",
-                    labelClass: "marker-labels"
-                  },
-                  selectDevice: scope.selectDevice
-                });
-              }
+            var map = new google.maps.Map(d3.select("#map").node(), {
+              zoom: 8,
+              center: new google.maps.LatLng(45, -73),
+              mapTypeId: google.maps.MapTypeId.TERRAIN
             });
-            scope.filteredMarkers = scope.markers;
+
+            var data = convertDevicesToMarkers(scope.devices);
+            var overlay = new google.maps.OverlayView();
+            var bounds = new google.maps.LatLngBounds();
+
+            Object.keys(data).forEach(function(d) {
+              var datapoint = data[d];
+              bounds.extend(new google.maps.LatLng(datapoint[1], datapoint[0]));
+            });
+
+            // Add the container when the overlay is added to the map.
+            overlay.onAdd = function() {
+              var layer = d3.select(this.getPanes().overlayMouseTarget).append("div")
+                  .attr("class", "locations");
+
+              // Draw each marker as a separate SVG element.
+              overlay.draw = function() {
+                var projection = this.getProjection(),
+                    padding = 15;
+
+                var markers = layer.selectAll("svg")
+                    .data(d3.entries(data))
+                    .each(transform) // update existing markers
+                  .enter().append("svg:svg")
+                    .each(transform)
+
+                // Add a circle.
+                markers.append("svg:circle")
+                    .attr("r", 7.5)
+                    .attr("cx", padding)
+                    .attr("cy", padding)
+                    .style("cursor", "pointer")
+                    .on('mouseover', function(d) {
+                      d3.select(this).transition().duration(500).attr('r', 10);
+                    })
+                    .on('mouseout', function (d) {
+                      d3.select(this).transition().duration(500).attr('r', 7.5);
+                    })
+                    .on('click', function(d) {
+                      scope.selectDevice(d.value[2]);
+                    });
+
+                function transform(d) {
+                  var status = d.value[2].status;
+                  d = new google.maps.LatLng(d.value[1], d.value[0]);
+                  d = projection.fromLatLngToDivPixel(d);
+                  d.status = status;
+
+                  return d3.select(this)
+                      .style("left", (d.x - padding) + "px")
+                      .style("top", (d.y - padding) + "px")
+                      .attr('class', 'marker-' + status);
+                }
+              };
+            };
+
+            // Bind our overlay to the map…
+            overlay.setMap(map);
+            map.fitBounds(bounds);
           }
         }
-
-        scope.onMarkerClicked = function onMarkerClicked(marker) {
-          marker.showWindow = true;
-          scope.$apply();
-        };
 
         scope.selectDevice = function(device) {
           $state.go('device', {deviceId: (typeof device === 'string') ? device : device.id}, {reload: true});
         };
+
+        function convertDevicesToMarkers(devices) {
+          var data = {};
+          devices.forEach(function(device) {
+            if (device.location) {
+              var lastCheckinTimeInSeconds = new Date(device.lastCheckin).getTime() / 1000;
+              var nowInSeconds = new Date().getTime() / 1000;
+
+              var gracePeriodInSeconds = 60;
+              var hasCheckedInOnTime = (lastCheckinTimeInSeconds + device.checkinInterval + gracePeriodInSeconds) > nowInSeconds;
+
+              var allCamerasOnline = true;
+              if (device.cameras) {
+                for (var k = 0; k < device.cameras.length; k++) {
+                  var camera = device.cameras[k];
+                  if (camera.status != 'online') {
+                    allCamerasOnline = false;
+                    break;
+                  }
+                }
+              }
+
+              if (hasCheckedInOnTime) {
+                if (allCamerasOnline) {
+                  device.status = 'green';
+                } else {
+                  device.status = 'yellow';
+                }
+              } else {
+                device.status = 'red';
+              }
+
+              var id = device.id;
+              var lat = device.location.lat;
+              var lng = device.location.lng;
+              data[id] = [lng, lat, device];
+            }
+          });
+
+          return data;
+        }
       }
     }
   });
