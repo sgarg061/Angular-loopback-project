90ac4a0d03292fee4367ec80992d1ac5e719cafd
Chris Whiten <chris.whiten@gmail.com>
2016-02-23 16:22:31 -0500

    Fix reseller permissions

--- common/models/reseller.js
+++ common/models/reseller.js
@@ -2,6 +2,7 @@ var uuid = require('node-uuid');
 var authService = require('../../server/services/authService');
 var logger = require('../../server/logger');
 var loopback = require('loopback');
+var _ = require('lodash');
 
 module.exports = function(Reseller) {
 
@@ -22,13 +23,13 @@ module.exports = function(Reseller) {
                 error.statusCode = 400;
                 next(error);
               } else {
-                                // reseller is valid...
-                                createResellerUser(ctx.instance, function (err) {
-                                  ctx.instance.unsetAttribute('password');
-                                  next(err);
-                                });
-                              }
-                            });
+                // reseller is valid...
+                createResellerUser(ctx.instance, function (err) {
+                  ctx.instance.unsetAttribute('password');
+                  next(err);
+                });
+              }
+            });
           }
         } else {
           next();
@@ -119,85 +120,96 @@ module.exports = function(Reseller) {
 
 function resellerAccessPermissions(ctx, next) {
   var context = loopback.getCurrentContext();
-  if (context && context.get('jwt')) {
+  if (!context) {
+    // triggered by a test or server-side code
+    return next();
+  }
+
+  if (context.get('jwt')) {
     var resellerId = context.get('jwt').resellerId;
     var cloudId = context.get('jwt').cloudId;
     var userType = context.get('jwt').userType;
     var customerId = context.get('jwt').tenantId;
 
-    if (userType === 'solink') {
-      next();
-    } else if (customerId) {
-            // only allow read for reseller
-            loopback.getModel('Customer').findById(customerId, function (err, customer) {
-              if (err) {
-                next(err);
-              } else {
-                if (ctx.query.where) {
-                  ctx.query.where.id = customer.resellerId;
-                } else {
-                  ctx.query.where = {
-                    id: customer.resellerId
-                  };
-                }
-                next();
-              }
-            });
-          } else if (resellerId) {
-            if (ctx.isNewInstance) {
-                // resellers cannot create new resellers
-                var e = new Error('Unauthorized');
-                e.statusCode = 401;
-                next(e);
-              } else {
-                // resellers cannot change reseller or cloud IDs
-                if (ctx.data && ctx.data.id) {
-                  delete ctx.data.id;
-                }
-              }
-              next();
-            } else if (cloudId) {
-              if (ctx.isNewInstance) {
-                // cloud users can only create resellers under their own domain
-                ctx.instance.cloudId = cloudId;
-                ctx.instance.id = null;
-              } else {
-                // cloud users cannot modify the reseller or cloud id
-                if (ctx.data && ctx.data.cloudId) {
-                  delete ctx.data.cloudId;
-                }
-                if (ctx.data && ctx.data.id) {
-                  delete ctx.data.id;
-                }
-              }
-              next();
-            } else {
-              var error = new Error('Unauthorized');
-              error.statusCode = 401;
-              next(error);
-            }
+    switch (userType) {
+      case 'solink':
+        return next();
+      case 'admin':
+      case 'connect':
+      case 'standard':
+        console.log('i am a connect...');
+        // only allow read for reseller
+        loopback.getModel('Customer').findById(customerId, function (err, customer) {
+          console.log('here is my customer...', customer);
+          if (err) {
+            return next(err);
           } else {
-            next();
+            var merged = _.merge({temp: 'temp'}, ctx.query.where);
+            ctx.query.where = _.merge(merged, {id: customer.resellerId});
+            delete ctx.query.where.temp;// this is a weird work around.  _.merge not working as expected
+            return next();
+          }
+        });
+        break;
+      case 'reseller':
+        if (ctx.isNewInstance) {
+          // resellers cannot create new resellers
+          var e = new Error('Unauthorized');
+          e.statusCode = 401;
+          return next(e);
+        } else {
+          // resellers cannot change reseller or cloud IDs
+          if (ctx.data && ctx.data.id) {
+            delete ctx.data.id;
+          }
+          ctx.query.where = _.merge(ctx.query.where, {id: resellerId});
+        }
+        return next();
+
+      case 'cloud':
+        if (ctx.isNewInstance) {
+          // cloud users can only create resellers under their own domain
+          ctx.instance.cloudId = cloudId;
+          ctx.instance.id = null;
+        } else {
+          // cloud users cannot modify the reseller or cloud id
+          if (ctx.data && ctx.data.cloudId) {
+            delete ctx.data.cloudId;
+          }
+          if (ctx.data && ctx.data.id) {
+            delete ctx.data.id;
           }
         }
+        return next();
+      default:
+        var error = new Error('Unauthorized');
+        error.statusCode = 401;
+        return next(error);
+    }
+  } else {
+    var unauthError = new Error('Unauthorized');
+    unauthError.statusCode = 401;
+    return next(unauthError);
+  }
+}
 
-        function createResellerUser(reseller, next) {
-          var userData = {
-            userType: 'reseller',
-            resellerId: reseller.id,
-            email_verified: true
-          };
+function createResellerUser(reseller, next) {
+  var userData = {
+    userType: 'reseller',
+    resellerId: reseller.id,
+    email_verified: true
+  };
 
-          authService.createUser(reseller.email, reseller.password, userData, function (err, res) {
-            if (err) {
-              logger.error('Could not create reseller user');
-              logger.error(err);
-              next(err);
-            } else {
-              logger.info('Created new reseller user ' + reseller.email);
-              logger.info(res);
-              reseller.unsetAttribute('password');
-              next();
-            }
-          });
-        }
+  authService.createUser(reseller.email, reseller.password, userData, function (err, res) {
+    if (err) {
+      logger.error('Could not create reseller user');
+      logger.error(err);
+      next(err);
+    } else {
+      logger.info('Created new reseller user ' + reseller.email);
+      logger.info(res);
+      reseller.unsetAttribute('password');
+      next();
+    }
+  });
+}
