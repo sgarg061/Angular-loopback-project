4299c3b48fed7aab5db75d646479dde7a78e5c3c
Chris Whiten <chris.whiten@gmail.com>
2016-02-23 08:21:23 -0500

    Clean up switching logic

--- common/models/customer.js
+++ common/models/customer.js
@@ -63,55 +63,63 @@ module.exports = function(Customer) {
       return next(); // test access
     } else if (!context.get('jwt') || context.get('jwt').userType === 'solink') {
       // querying as a test or as solink
-      next();
-    } else if (context.get('jwt') && context.get('jwt').tenantId) {
-      // querying with a customer's credentials
-      var tenantId = context.get('jwt').tenantId;
-      if (ctx.query.where) {
-        ctx.query.where.id = tenantId;
-      } else {
-        ctx.query.where = {
-          id: tenantId
-        };
-      }
-      next();
-    } else if (context.get('jwt') && context.get('jwt').resellerId) {
-      var resellerId = context.get('jwt').resellerId;
-      if (ctx.query.where) {
-        ctx.query.where.resellerId = resellerId;
-      } else {
-        ctx.query.where = {
-          resellerId: resellerId
-        };
-      }
-      next();
-    } else if (context.get('jwt') && context.get('jwt').cloudId) {
-      var cloudId = context.get('jwt').cloudId;
-      var Reseller = Customer.app.models.Reseller;
+      return next();
+    }
 
-      Reseller.find({where: {cloudId: cloudId}}, function (err, res) {
-        if (err) {
-          logger.error('Error querying resellers with cloud id ' + cloudId);
-          logger.error(err);
-          next(err);
+    switch (context.get('jwt').userType) {
+      case 'admin':
+      case 'standard':
+        // querying with a customer's credentials
+        var tenantId = context.get('jwt').tenantId;
+        if (ctx.query.where) {
+          ctx.query.where.id = tenantId;
         } else {
-          var ids = [];
-          for (var i = 0; i < res.length; i++) {
-            ids.push(res[i].id);
-          }
-
-          if (ctx.query.where) {
-            ctx.query.where.resellerId = {inq: ids};
-          } else {
-            ctx.query.where = {
-              resellerId: {
-                inq: ids
-              }
-            };
-          }
-          next();
+          ctx.query.where = {
+            id: tenantId
+          };
         }
-      });
+        next();
+        break;
+      case 'reseller':
+        console.log('reseller!', context.get('jwt').resellerId);
+        var resellerId = context.get('jwt').resellerId;
+        if (ctx.query.where) {
+          ctx.query.where.resellerId = resellerId;
+        } else {
+          ctx.query.where = {
+            resellerId: resellerId
+          };
+        }
+        next();
+        break;
+      case 'cloud':
+        var cloudId = context.get('jwt').cloudId;
+        var Reseller = Customer.app.models.Reseller;
+
+        Reseller.find({where: {cloudId: cloudId}}, function (err, res) {
+          if (err) {
+            logger.error('Error querying resellers with cloud id ' + cloudId);
+            logger.error(err);
+            next(err);
+          } else {
+            var ids = [];
+            for (var i = 0; i < res.length; i++) {
+              ids.push(res[i].id);
+            }
+
+            if (ctx.query.where) {
+              ctx.query.where.resellerId = {inq: ids};
+            } else {
+              ctx.query.where = {
+                resellerId: {
+                  inq: ids
+                }
+              };
+            }
+            next();
+          }
+        });
+        break;
     }
   });
 
