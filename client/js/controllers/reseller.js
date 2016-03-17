fce2a4a4d9c0250f550437bcc3e93f89f4bc2432
Abdul Rehman <abdul_rehman1996@hotmail.com>
2016-03-17 16:56:29 -0400

    visually imporved sidebar

--- client/js/controllers/reseller.js
+++ client/js/controllers/reseller.js
@@ -26,10 +26,18 @@ angular
         if (newValue) {
           var id = $scope.reseller.id;
           if (newValue.eventServerUrl !== oldValue.eventServerUrl) {
-            updateReseller(id, {eventServerUrl: newValue.eventServerUrl}, 'Event server URL has been updated');
+            if (newValue.eventServerUrl === ''){
+              updateReseller(id, {eventServerUrl: null}, 'Event server URL has been updated'); 
+            } else {
+              updateReseller(id, {eventServerUrl: newValue.eventServerUrl}, 'Event server URL has been updated');
+            }
           }
           if (newValue.imageServerUrl !== oldValue.imageServerUrl) {
-            updateReseller(id, {imageServerUrl: newValue.imageServerUrl}, 'Image server URL has been updated');
+            if (newValue.imageServerUrl === ''){
+              updateReseller(id, {imageServerUrl: null}, 'Image server URL has been updated');
+            } else { 
+              updateReseller(id, {imageServerUrl: newValue.imageServerUrl}, 'Image server URL has been updated');
+            }
           }
           if (newValue.signallingServerUrl !== oldValue.signallingServerUrl) {
             updateReseller(id, {signallingServerUrl: newValue.signallingServerUrl}, 'Signalling server has been updated');
@@ -60,7 +68,8 @@ angular
       Reseller.prototype$updateAttributes({id: id}, changedDictionary)
         .$promise.then(function(reseller) {toastr.info(' ' + message);},
           function (res) {
-        toastr.error(res.data.error.message, 'Error');
+            console.log(res)
+        toastr.error(res.statusText, 'Error Invalid Value');
       });
     }
 
@@ -93,7 +102,7 @@ angular
           if(!_.isEmpty(resellers)){
             $scope.reseller = resellers[0];
 
-
+            $scope.imageServerUrl = resellers[0].imageServerUrl;
             $scope.cloudId = resellers[0].cloud.id;
             $scope.cloud = resellers[0].cloud;
             $scope.resellerId = resellers[0].id;
@@ -466,7 +475,7 @@ angular
       .then(function(data) {
         toastr.success('Unassigned filter successfully!', 'Filter Unassigned')
       }, function (res) {
-        toastr.error(res.data.error.message, 'Error');
+        toastr.error(res.data.error.message, 'Errors');
       });
     }
 
